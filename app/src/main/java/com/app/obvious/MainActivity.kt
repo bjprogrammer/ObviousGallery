1442a38f77f973c588e266f81ad33251f25c1dee
Bobby Jasuja <bobbyjasuja@gmail.com>
2022-09-19 19:10:12 +0530

    Added base activity

--- app/src/main/java/com/app/obvious/MainActivity.kt
+++ app/src/main/java/com/app/obvious/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.app.obvious
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.app.obvious.utils.ConnectivityReceiver
+import com.app.obvious.utils.showToast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ConnectivityReceiver.ConnectivityReceiverListener {
+    private var isInternetAvailable = true
+    private lateinit var intentFilter: IntentFilter
+    private lateinit var receiver: ConnectivityReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Initialize Connectivity receiver to update internet status
+        intentFilter = IntentFilter()
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
+        receiver = ConnectivityReceiver()
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(receiver, intentFilter)
+        ConnectivityReceiver.connectivityReceiverListener = this
+    }
+
+    //Checking internet isInternetAvailable using broadcast receiver
+    override fun onNetworkConnectionChanged(isConnected: Boolean) {
+        if (isInternetAvailable != isConnected) {
+            if (isConnected) {
+                showToast("Connected to internet")
+            } else {
+                showToast("No Internet")
+            }
+        }
+        isInternetAvailable = isConnected
+    }
+
+    override fun onStop() {
+        try {
+            unregisterReceiver(receiver)
+        } catch (_: Exception) { }
+        super.onStop()
     }
 }
---
+++ app/src/main/java/com/app/obvious/utils/ConnectivityReceiver.kt
@@ -0,0 +1,42 @@
+package com.app.obvious.utils
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+//Checks for internet connection
+class ConnectivityReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, arg: Intent) {
+        connectivityReceiverListener?.onNetworkConnectionChanged(isNetworkAvailable(context))
+    }
+
+    private fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val nw = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                //for other device how are able to connect with Ethernet
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                //for check internet over Bluetooth
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                else -> false
+            }
+        } else {
+            return connectivityManager.activeNetworkInfo?.isConnected ?: false
+        }
+    }
+
+    interface ConnectivityReceiverListener {
+        fun onNetworkConnectionChanged(isConnected: Boolean)
+    }
+
+    companion object {
+        var connectivityReceiverListener: ConnectivityReceiverListener? = null
+    }
+}
